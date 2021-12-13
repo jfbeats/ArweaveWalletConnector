e7608392b03a63b2b44c13490897299438468c22
JF <simplyhiphopmusic@gmail.com>
2021-12-13 16:39:59 -0500

    -

--- src/ArweaveWebWallet.ts
+++ src/ArweaveWebWallet.ts
@@ -1,29 +1,27 @@
 import Connector from './Connector'
 import { is } from 'typescript-is'
+import type { AsProvider, AsVerifier, Override } from './types'
 import type TransactionInterface from 'arweave/web/lib/transaction'
 import type { ApiConfig } from 'arweave/web/lib/api'
-import type { Verifier } from './types'
 
 
 
-interface TxOverrides {
+interface Tx extends Override<TransactionInterface, {
 	tags: { name: string, value: string }[]
-}
-interface Tx extends TxOverrides, Omit<TransactionInterface, keyof TxOverrides> {}
+}> {}
 
 
 
-export interface ArweaveAPI {
+export interface ArweaveInterface {
 	getPublicKey(): Promise<string>
 	getArweaveConfig(): Promise<Omit<ApiConfig, 'logger'> & { logger?: any }>
 	signTransaction(tx: TransactionInterface, options?: object): Promise<TransactionInterface>
 	sign(message: string, options?: object): Promise<string>
 	decrypt(message: string, options?: object): Promise<string>
 }
-interface ProviderOverrides {
+export interface ArweaveProviderInterface extends AsProvider<ArweaveInterface, {
 	signTransaction(tx: TransactionInterface, options?: object): { id: string, owner?: string, tags?: { name: string, value: string }[], signature: string, fee?: string }
-}
-export interface ArweaveProviderAPI extends ProviderOverrides, Omit<ArweaveAPI, keyof ProviderOverrides> {}
+}> {}
 
 
 
@@ -31,20 +29,20 @@ type Emitting = {}
 
 
 
-export class ArweaveWebWallet extends Connector<Emitting> implements ArweaveAPI {
+export class ArweaveWebWallet extends Connector<Emitting> implements ArweaveInterface {
 	constructor(appInfo?: { name?: string, logo?: string }, url?: string,) {
 		super({ protocol: 'arweave', version: '1.0.0' }, { ...appInfo }, url)
 	}
 
 	async getPublicKey() {
 		const res = await this.postMessage('getPublicKey')
-		if (!is<Awaited<ReturnType<ArweaveProviderAPI['getPublicKey']>>>(res)) { throw 'TypeError' }
+		if (!is<ReturnType<ArweaveProviderInterface['getPublicKey']>>(res)) { throw 'TypeError' }
 		return res
 	}
 
 	async getArweaveConfig() {
 		const res = await this.postMessage('getArweaveConfig')
-		if (!is<Awaited<ReturnType<ArweaveProviderAPI['getArweaveConfig']>>>(res)) { throw 'TypeError' }
+		if (!is<ReturnType<ArweaveProviderInterface['getArweaveConfig']>>(res)) { throw 'TypeError' }
 		delete res.logger
 		return res
 	}
@@ -53,7 +51,7 @@ export class ArweaveWebWallet extends Connector<Emitting> implements ArweaveAPI 
 		// check if tx is Transaction or object
 		const { data, chunks, ...txHeader } = tx // todo transfer data separately?
 		const res = await this.postMessage('signTransaction', [txHeader, options])
-		if (!is<Awaited<ReturnType<ArweaveProviderAPI['signTransaction']>>>(res)) { throw 'TypeError' }
+		if (!is<ReturnType<ArweaveProviderInterface['signTransaction']>>(res)) { throw 'TypeError' }
 		tx.setSignature({ id: res.id, owner: res.owner || tx.owner, signature: res.signature }) // todo res.tags
 		if (res.fee) { tx.fee = res.fee }
 		return tx
@@ -68,20 +66,20 @@ export class ArweaveWebWallet extends Connector<Emitting> implements ArweaveAPI 
 
 	async sign(message: string, options?: object) {
 		const res = await this.postMessage('sign', [message, options])
-		if (!is<Awaited<ReturnType<ArweaveProviderAPI['sign']>>>(res)) { throw 'TypeError' }
+		if (!is<ReturnType<ArweaveProviderInterface['sign']>>(res)) { throw 'TypeError' }
 		return res
 	}
 
 	async decrypt(message: string, options?: object) {
 		const res = await this.postMessage('decrypt', [message, options])
-		if (!is<Awaited<ReturnType<ArweaveProviderAPI['decrypt']>>>(res)) { throw 'TypeError' }
+		if (!is<ReturnType<ArweaveProviderInterface['decrypt']>>(res)) { throw 'TypeError' }
 		return res
 	}
 }
 
 
 
-export class ArweaveWebWalletVerifier implements Verifier<ArweaveAPI> {
+export class ArweaveVerifierInterface implements AsVerifier<ArweaveInterface> {
 	getPublicKey() { return true }
 	getArweaveConfig() { return true }
 	signTransaction(tx: Tx, options?: object | undefined): boolean {
--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,13 @@
 export type Flatten<T> = T extends Record<string, any> ? { [k in keyof T]: T[k] } : never
 export type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never
-export type ParamsVerifier<T extends (...a: any) => any> = (...a: Parameters<T>) => boolean
-export type Verifier<C> = { [Property in keyof C]: C[Property] extends (...a: any) => any ? ParamsVerifier<C[Property]> : never }
+export type Override<T, U> = Omit<T, keyof U> & U
+
+type ProvideResult<T> = T extends (...a: any) => any ? (...a: Parameters<T>) => Awaited<ReturnType<T>> : never
+type Provider<C> = { [Property in keyof C]: ProvideResult<C[Property]> }
+export type AsProvider<Base, Overrides> = Provider<Override<Base, Overrides>>
+
+type VerifyParams<T> = T extends (...a: any) => any ? (...a: Parameters<T>) => boolean : never
+export type AsVerifier<C> = { [Property in keyof C]: VerifyParams<C[Property]> }
 
 export type AppInfo = {
 	name?: string
